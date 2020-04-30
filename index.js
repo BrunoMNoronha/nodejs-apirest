63034715bb935ad2a9a1020a2bf5a1b1882cdbc2
Bruno M Noronha <bruno.developer@pm.me>
2020-04-30 19:03:40 -0300

    Refatorando rotas

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir')
 
+// iniciando o App
 const app = express();
 
+// iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi',
 { useUnifiedTopology: true , useNewUrlParser: true });
 
+// Injetando models
 requireDir('./src/models');
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-});
+// Primeira rota, redirecionando rotas
+app.use('/api', require('./src/routes'))
 
 app.listen(3001);
