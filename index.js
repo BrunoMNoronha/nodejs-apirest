b3d2387ffca8459280c040df87939dad93a7c05d
Bruno M Noronha <bruno.developer@pm.me>
2020-04-30 17:39:28 -0300

    Criando models e fazendo autoload com requireDir

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir')
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useUnifiedTopology: true , useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+{ useUnifiedTopology: true , useNewUrlParser: true });
+
+requireDir('./src/models');
 
 app.get('/', (req, res) => {
     res.send("Hello World")
