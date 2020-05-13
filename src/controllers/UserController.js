c14f32ac17c00c40fee1034238838ef66031f3ad
Bruno M Noronha <bruno.developer@pm.me>
2020-05-12 22:31:13 -0300

    corrigindo o metodo index

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 module.exports = {
-    async index(res) {
+    async index(req, res) {
         const users = await User.find();
         return res.json(users);
     },
+
 
     async show(req, res) {
         const user = await User.findById(req.params.id);
